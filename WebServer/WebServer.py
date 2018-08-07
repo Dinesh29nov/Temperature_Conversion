c303ee6b11320f662780455bc5652a513c1ce91c
Dinesh Buswala <38141621+Dinesh29nov@users.noreply.github.com>
2018-08-07 18:16:15 +0530

    Update WebServer.py
    Commit After Subbu's feedback on 7th August 2017

--- WebServer/WebServer.py
+++ WebServer/WebServer.py
@@ -1,15 +1,5 @@
-######### Necessary packagees ############
+#########      Necessary packagees        ############
 
-try:
-    from Tkinter import *
-    import Tkinter as tk
-
-except ImportError:
-    from tkinter import *
-    import tkinter as tk
-
-import tkMessageBox
-import tkSimpleDialog
 from flask import Flask, redirect, url_for
 from flask import jsonify
 from flask import Flask, render_template
@@ -17,108 +7,82 @@ from flask.ext.socketio import SocketIO, emit
 from flask import Flask, Response, request
 import flask
 
-####################################################
+######################################################
 
 
 
 #####################################################
 ################   SERVER SIDE       ################
-################    MAIN LOGIC       ################
+################   MAIN LOGIC        ################
 #####################################################
 
 
 
 
-input=0
+inputs=0
 vara=0
 app = Flask(__name__)
+#routing or mapping the URL
 @app.route('/', methods=['GET','POST']) # both redirect to same page i.e., home page
-@app.route('/Convert', methods=['GET','POST'])
-def Convert():
-    global input, vara
-    if(request.method=='POST'): # match on the request is POST
-        input=request.data #data format is string here
-        if(input.isspace()):
-            root = tk.Tk()  # to hide the root window 
-            root.withdraw() 
-            tkMessageBox.showinfo(title="Server-Warning!", message="User doesn't enter any value : ENTER SOME DATA INSTEAD OF SPACE")
-            return redirect(url_for('Convert'))
-        input=input.replace(" ","")
-        if(input!='' ): #checking value is not empty : Action No action
-            lastindex=input[-1]
-            if(lastindex=='f' or lastindex== 'F' ): #matching the check mark inserted by client for appropiate conditions
-                input=input[:-1]  # removing the check mark to extract original data 
-                # redirecting
-                return redirect(url_for('Convertfahrenheit',para=input))
-            if(lastindex=='c' or lastindex=='C'): #matching the check mark inserted by client for appropiate conditions
-                input=input[:-1]  # removing the check mark to extract original data
-                # redirecting
-                return redirect(url_for('Convertcelsius', para=input)) # redirecting
-            if(lastindex=='k' or lastindex=='K'): #matching the check mark inserted by client for appropiate conditions
-                input=input[:-1]  # removing the check mark to extract original data
-                # redirecting
-                return redirect(url_for('Convertkelvin', para=input)) # redirecting
-            if(lastindex=='r' or lastindex=='R'): #matching the check mark inserted by client for appropiate conditions
-                input=input[:-1]  # removing the check mark to extract original data
-                # redirecting
-                return redirect(url_for('Convertrankine', para=input))  # redirecting
-            if(lastindex!='f' and lastindex!= 'F' and lastindex!='c' and lastindex!='C' and lastindex!='k' and lastindex!='K' and lastindex!='r' and lastindex!='R'):
-                
-                # redirecting
-                return redirect(url_for('ConvertTemperature', para=input))
+@app.route('/convert', methods=['GET','POST'])
+def convert():
     return "Temperature Conversion Program  " ### printing the values on the web page
 
-@app.route('/Convertfahrenheit/<para>', methods=['GET','POST'])  
-def Convertfahrenheit(para):
+@app.route('/convert/fahrenheit/<para>', methods=['GET','POST'])  
+def fahrenheit(para):
      ### conversion logic refer : https://en.wikipedia.org/wiki/Conversion_of_units_of_temperature  ### 
     if(para.isdigit()): #checking the number is digit or not 
         cel= (float(para)-32.0) * (5.0/9.0)
         ranki=float(para) + 459.67
         kel= (float(para) + 459.67) *(5.0/9.0)
-        return "celsius={} rankine={}, kelvin={} " .format(cel, ranki, kel) ### printing the values on the web page
+        return jsonify(celsius=cel, rankine=ranki, kelvin=kel)
     try: #checking number is float or not
         cel= (float(para)-32.0) * (5.0/9.0)
         ranki=float(para) + 459.67
         kel= (float(para) + 459.67) *(5.0/9.0)
-        return "celsius={} rankine={}, kelvin={} " .format(cel, ranki, kel) ### printing the values on the web page
+        return jsonify(celsius=cel, rankine=ranki, kelvin=kel)
     except ValueError: #handling exception if data is not float
         return "Please enter some integer or real number !! "    
 
-@app.route('/Convertcelsius/<para>', methods=['GET','POST'])  
-def Convertcelsius(para):  
+@app.route('/convert/celsius/<para>', methods=['GET','POST'])  
+def celsius(para):  
       ### conversion logic refer : https://en.wikipedia.org/wiki/Conversion_of_units_of_temperature  ### 
     if(para.isdigit()):
-        farh=float(para) * (9.0/5.0) +32.0
+        farh=float(para) * (9.0/5.0) + 32.0
         ranki=(float(para)+273.15)*(9.0/5.0)
         kel=  float(para) + 273.15
-        return "rankine={} fahrenheit={}, kelvin={} " .format(ranki, farh, kel) ### printing the values on the web page
+        return jsonify(rankine=ranki, kelvin=kel, fahrenheit=farh)
+        # "\"rankine\": {}, \"fahrenheit\": {}, \"kelvin\": {} " .format(ranki, farh, kel) ### printing the values on the web page
     try:
         farh=float(para) * (9.0/5.0) +32.0
         ranki=(float(para)+273.15)*(9.0/5.0)
         kel=  float(para) + 273.15
-        return "rankine={} fahrenheit={}, kelvin={} " .format(ranki, farh, kel) ### printing the values on the web page
+        return jsonify(rankine=ranki, kelvin=kel, fahrenheit=farh)
+        # "\"rankine\": {}, \"fahrenheit\": {}, \"kelvin\": {} " .format(ranki, farh, kel) ### printing the values on the web page
     except ValueError:
         return "Please enter some integer or real number !! "    
 
-@app.route('/Convertrankine/<para>',methods=['GET','POST'])  
-def Convertrankine(para):
+@app.route('/convert/rankine/<para>',methods=['GET','POST'])  
+def rankine(para):
       ### conversion logic refer : https://en.wikipedia.org/wiki/Conversion_of_units_of_temperature  ### 
-    if(para.isdigit()): 
+    if(para.isdigit()):
         cel  = (float(para) - 491.67) * (5.0/9.0)   
         fah= float(para) - 459.67    
         kel = float(para) * (5.0/9.0)   
-        return "celsius={} fahrenheit={}, kelvin={} " .format(cel, fah, kel) ### printing the values on the web page
+        return jsonify(fahrenheit=fah, celsius=cel, kelvin=kel)
+        #"\"celsius\": {}, \"fahrenheit\": {}, \"kelvin\": {} " .format(cel, fah, kel) ### printing the values on the web page
     try:
         cel  = (float(para) - 491.67) * (5.0/9.0)   
         fah= float(para) - 459.67    
         kel = float(para) * (5.0/9.0)   
-        return "celsius={} fahrenheit={}, kelvin={} " .format(cel, fah, kel) ### printing the values on the web page
+        return jsonify(fahrenheit=fah, celsius=cel, kelvin=kel)
+        #"\"celsius\": {}, \"fahrenheit\": {}, \"kelvin\": {} " .format(cel, fah, kel) ### printing the values on the web page
     except ValueError:
         return "Please enter some integer or real number !! "    
 
 
-@app.route('/Convertkelvin/<para>', methods=['GET','POST'])  
-def Convertkelvin(para):
+@app.route('/convert/kelvin/<para>', methods=['GET','POST'])  
+def kelvin(para):
       ### conversion logic refer : https://en.wikipedia.org/wiki/Conversion_of_units_of_temperature  ### 
 
     if(para.isdigit()):
@@ -126,27 +90,17 @@ def Convertkelvin(para):
         fah= (float(para))*(9.0/5.0)- 459.67
         ranki=float(para)*(9.0/5.0)
               
-        return "celsius={} fahrenheit={}, rankine={} " .format(cel, fah, ranki)  ### printing the values on the web page
+        return jsonify(celsius=cel, fahrenheit=fah, rankine=ranki)
+        # "\"celsius\": {}, \"fahrenheit\": {}, \"rankine\": {} " .format(cel, fah, ranki)  ### printing the values on the web page
     try:
         cel = float(para) - float(273.15) 
         fah= (float(para))*(9.0/5.0)- 459.67
         ranki=float(para)*(9.0/5.0)
               
-        return "celsius={} fahrenheit={}, rankine={} " .format(cel, fah, ranki)  ### printing the values on the web page
+        return jsonify(celsius=cel, fahrenheit=fah, rankine=ranki)
     except ValueError:
         return "Please enter some integer or real number !! "  
 
-
-
-@app.route('/ConvertTemperature/<para>', methods=['GET','POST'])  
-def ConvertTemperature(para):
-    return "Please enter the temperature in some valid temperature unit !! "
-
-
-
 ####### main function to operate the web server at local host (127.0.0.1) #######
 if __name__ == "__main__":
-    app.run(host="127.0.0.1")
-
-
-
+    app.run(host="localhost") # Debug= True when u are in developer mode
